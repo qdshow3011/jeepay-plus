d5281f6ac61920969942f397b79d4264e02aa08c
qdshow3011 <qdshow@gmail.com>
2026-07-08 13:25:50 +0000

    feat: Coolify服务器安装问题排查与修复
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- jeepay-ui/docker-entrypoint.sh
+++ jeepay-ui/docker-entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-envsubst '${BACKEND_HOST},${PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+set -e
+
+if [ -z "$BACKEND_HOST" ]; then
+  echo "ERROR: BACKEND_HOST environment variable is required" >&2
+  exit 1
+fi
+
+envsubst '${BACKEND_HOST}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+
 exec nginx -g "daemon off;"
